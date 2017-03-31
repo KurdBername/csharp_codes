8bdc417201859d48c03fc59486d00094923985d7
kurdbername <hassanxkalo@gmail.com>
2017-03-31 21:03:08 +0300

    First Csharp program
    this is my first Csharp program

---
+++ Firstprogram/Firstprogram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//this is my first program in c#
+            Console.WriteLine("HelloWorld");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Firstvarible/Firstvarible/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstvarible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("My Varible Value =(0)",x);
+            Console.ReadKey();
+        }
+    }
+}
