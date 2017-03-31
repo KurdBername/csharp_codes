50d77eb3160ffedf6bb068c0b029fcf6217c43a2
kurdbername <hassanxkalo@gmail.com>
2017-03-31 22:53:53 +0300

    float-point first
    float-point

---
+++ Float_point data types/Float_point data types/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Float_point_data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+            Console.WriteLine("Min value = (0) Max val =(1)",flMin , flMax);
+            Console.ReadKey();
+        }
+    }
+}
