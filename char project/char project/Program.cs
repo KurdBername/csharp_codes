d0beb813f91786d4b45f514c5c2b83be0e5ce745
kurdbername <hassanxkalo@gmail.com>
2017-04-03 23:50:12 +0300

    char codes test
    a test for codes

---
+++ char project/char project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace char_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int charMin = Char.MinValue;
+            int charMax = Char.MaxValue;
+
+           Char myVar = 'G';
+            int myVarValue = 'G';
+            Console.WriteLine("Min char ={0} Max char = {1} my character = {2} my Variable Number = {3}",charMin ,charMax ,myVar ,myVarValue);
+            string mystring = "this is my program ";
+            Console.WriteLine("\nmystring is  {0}",mystring);
+            Console.ReadKey();
+        }
+    }
+}
